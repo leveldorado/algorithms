11fcee9197a1f6f52f69dfbcce83b83e772cd138
Vasyl Osypchuk <leveldorado@gmail.com>
2019-11-05 23:11:59 +0200

    QuickSortMiddle

--- sort.go
+++ sort.go
@@ -6,12 +6,35 @@ func QuickSort(list []int) []int {
 	}
 	pivot := list[0]
 	var left, right []int
-	for _, el := range list[1:] {
-		if el < pivot {
-			left = append(left, el)
+	for i := range list {
+		if i == 0 {
 			continue
 		}
-		right = append(right, el)
+		if list[i] < pivot {
+			left = append(left, list[i])
+			continue
+		}
+		right = append(right, list[i])
+	}
+	return append(append(QuickSort(left), pivot), QuickSort(right)...)
+}
+
+func QuickSortMiddle(list []int) []int {
+	if len(list) < 2 {
+		return list
+	}
+	pivotI := len(list) / 2
+	var left, right []int
+	pivot := list[pivotI]
+	for i := range list {
+		if pivotI != i {
+			continue
+		}
+		if list[i] < pivot {
+			left = append(left, list[i])
+			continue
+		}
+		right = append(right, list[i])
 	}
 	return append(append(QuickSort(left), pivot), QuickSort(right)...)
 }
--- sort_test.go
+++ sort_test.go
@@ -1,6 +1,7 @@
 package algorithms
 
 import (
+	"math/rand"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -9,4 +10,31 @@ import (
 func TestQuickSort(t *testing.T) {
 	a := []int{3, 5, 12, 1, 8, 16}
 	require.Equal(t, []int{1, 3, 5, 8, 12, 16}, QuickSort(a))
+}
+
+func TestQuickSortMiddle(t *testing.T) {
+	a := []int{3, 5, 12, 1, 8, 16}
+	require.Equal(t, []int{1, 3, 5, 8, 12, 16}, QuickSortMiddle(a))
+}
+
+func BenchmarkQuickSort1000(b *testing.B) {
+	list := generateRandomNumbers(1000)
+	for n := 0; n < b.N; n++ {
+		QuickSort(list)
+	}
+}
+
+func BenchmarkQuickSortMiddle1000(b *testing.B) {
+	list := generateRandomNumbers(1000)
+	for n := 0; n < b.N; n++ {
+		QuickSortMiddle(list)
+	}
+}
+
+func generateRandomNumbers(size int) []int {
+	var list []int
+	for i := 0; i < size; i++ {
+		list = append(list, rand.Intn(100))
+	}
+	return list
 }
